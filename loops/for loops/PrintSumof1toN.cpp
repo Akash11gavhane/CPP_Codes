c70a50efc8717cb2365500146facbb0de906e917
Akash11gavhane <akash1106gavhane@gmail.com>
2023-06-09 20:10:29 +0530

    cpp start again

---
+++ loops/for loops/PrintSumof1toN.cpp
@@ -0,0 +1,25 @@
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+
+    int n ;
+    cin >> n ;
+
+    bool isPrime = 1 ; 
+
+    for(int i = 1 ; i <= n ; i++){
+
+        if(n % i == 0){
+            isPrime = 0 ;
+            break;
+        }
+    }
+
+    if(isPrime == 0 ){
+        cout << "Not a prime Number" << endl;
+    }
+    else{
+        cout << "prime Number " << endl ;  
+    }
+ }
